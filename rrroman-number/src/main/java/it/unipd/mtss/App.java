8c59d496e6e4e9ed3f8d7d91d3e8d4b8f77d9c9a
[marcogiacon04] <[marco.giacon.4@studenti.unipd.it]>
2025-05-10 11:30:58 +0200

    gggg

---
+++ rrroman-number/src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,12 @@
+//prova
+
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
